7a8177a9125006f9051c3bed7d18e96bc3769193
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2020-06-28 14:23:20 +0100

    first commit

---
+++ applogger.go
@@ -0,0 +1,76 @@
+package applogger
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+var (
+	generalLogger *log.Logger
+	errorLogger   *log.Logger
+)
+
+type AppLogger struct {
+	Path string
+}
+
+// logNDJOSNHTTP json format for logs in lib and controller packages
+type logNDJOSN struct {
+	PID        string    `json:"pid"`
+	Level      string    `json:"level"`
+	LogPackage string    `json:"package"`
+	LogFunc    string    `json:"func"`
+	Message    string    `json:"message"`
+	DOB        time.Time `json:"time"`
+}
+
+// logNDJOSNHTTP json format for logs in the main package
+type logNDJOSNHTTP struct {
+	PID        string    `json:"pid"`
+	Level      string    `json:"level"`
+	LogPackage string    `json:"package"`
+	LogFunc    string    `json:"func"`
+	Message    string    `json:"message"`
+	DOB        time.Time `json:"time"`
+	Code       int       `json:"code"`
+	Duration   float64   `json:"duration"`
+}
+
+func (r AppLogger) init() {
+	generalLog, err := os.OpenFile(r.Path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	generalLogger = log.New(generalLog, "", 0)
+	errorLogger = log.New(generalLog, "", 0)
+}
+
+// Log writting to a ndjson file logs for lib and controller packages
+func (r AppLogger) Log(level string, logPackage string, logFunc string, message string) {
+
+	s1 := time.Now()
+	u := uuid.Must(uuid.NewV4())
+
+	x := logNDJOSN{PID: u.String(), Level: level, LogPackage: logPackage, LogFunc: logFunc, Message: message, DOB: s1}
+	res2B, _ := json.Marshal(x)
+	generalLogger.Println(string(res2B))
+}
+
+// LogHTTP writting to a ndjson file logs for the main package
+// the difference is that we are recording the http status
+// and the duration of the request
+func (r AppLogger) LogHTTP(level string, logPackage string, logFunc string, message string, code int, duration float64) {
+
+	s1 := time.Now()
+	u := uuid.Must(uuid.NewV4())
+
+	x := logNDJOSNHTTP{PID: u.String(), Level: level, LogPackage: logPackage, LogFunc: logFunc, Message: message, DOB: s1, Code: code, Duration: duration}
+	res2B, _ := json.Marshal(x)
+	generalLogger.Println(string(res2B))
+}
---
+++ applogger_test.go
@@ -0,0 +1,45 @@
+package applogger
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	directoryPath := "./tmp"
+	filePath := directoryPath + "/log.ndjson"
+	os.MkdirAll(directoryPath, os.ModePerm)
+
+	okman := AppLogger{Path: filePath}
+	okman.init()
+	okman.Log("INFO", "main", "app", "This is a test")
+	okman.Log("ERROR", "main", "app", "Cannot open file")
+	okman.LogHTTP("INFO", "controller", "perform", "Response: { \"status\": 200}", 200, 0.74453)
+	okman.LogHTTP("ERROR", "controller", "perform", "Response: { \"status\": 500}", 500, 2.73353)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		errNDJSON := isJSON(fmt.Sprintf("%v", scanner.Text()))
+
+		if errNDJSON != nil {
+			os.RemoveAll(directoryPath)
+			t.Fatalf("line is not in a json format %s with error %s", scanner.Text(), errNDJSON)
+		}
+	}
+	os.RemoveAll(directoryPath)
+
+}
+
+func isJSON(s string) error {
+	var js map[string]interface{}
+	err := json.Unmarshal([]byte(s), &js)
+	return err
+}
